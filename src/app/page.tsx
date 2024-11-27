cd7858f8c7f3db64f3c54bbfce3e84dd191f2547
jvxz <tinycat2004@proton.me>
2024-11-26 23:50:54 -0500

    update Providers.tsx, page.tsx and tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useTheme } from "next-themes";
 import { Button } from "./components/Button";
 
 export default function Page() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Button
+        onPress={() => {
+          setTheme(theme === "dark" ? "light" : "dark");
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 }
