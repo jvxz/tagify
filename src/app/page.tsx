28c8ad093071467f511f39e64bfb236480173545
jvxz <tinycat2004@proton.me>
2024-11-27 21:14:14 -0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import FileTree from "./components/FileTree";
 import ThemeToggle from "./components/ThemeToggle";
 
 export default function Page() {
   return (
-    <div className="grid h-screen place-items-center">
-      <ThemeToggle />
-    </div>
+    <main className="flex h-screen flex-row bg-background">
+      <div className="h-full w-[300px] border-r border-border p-4 shadow-md">
+        <ThemeToggle />
+        <FileTree />
+      </div>
+      <div className="flex h-full flex-1 flex-col"></div>
+    </main>
   );
 }
