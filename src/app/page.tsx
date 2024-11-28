650876771a8250d93e7b1b879a3b8e64cd4578ed
jvxz <jamie@wisp.bio>
2024-11-27 19:14:05 -0500

    update 6 files and delete 1 file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-"use client";
-import { useTheme } from "next-themes";
-import { Button } from "./components/Button";
-
 export default function Page() {
-  const { theme, setTheme } = useTheme();
-
-  return (
-    <div className="bg-background text-foreground flex h-screen flex-col items-center justify-center">
-      <Button
-        onPress={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-        }}
-      >
-        Click me
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
