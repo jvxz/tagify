ab39d526d98999a8b5399fc46686fde89b10bde7
jvxz <tinycat2004@proton.me>
2024-11-30 13:33:51 -0500

    update 7 files

--- src/app/components/FileEditorPanel.tsx
+++ src/app/components/FileEditorPanel.tsx
@@ -2,15 +2,26 @@
 import useFileStore from "@/lib/store/files";
 import { Button } from "@/components/ui/button";
 import FileEditorPanelForm from "./FileEditorPanelForm";
-
 export default function FileEditorPanel() {
   const { selectedFile } = useFileStore();
+
+  function formatDuration(duration: number) {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration % 60)
+      .toString()
+      .padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  }
 
   return (
     <div className="flex flex-col">
       <div className="flex h-16 items-center justify-between border-b border-border/50 p-4">
         <p className="text-xl font-bold">
           {selectedFile?.name ? selectedFile.name : "no file selected"}
+        </p>
+        <p>
+          {selectedFile?.tags?.format?.duration &&
+            formatDuration(selectedFile.tags.format.duration)}
         </p>
         <div className="flex items-center gap-2">
           <Button isDisabled={!selectedFile} variant="destructive" size="sm">
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,8 +4,8 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Sidebar() {
   return (
-    <div className="flex h-screen w-20 flex-col items-center gap-4 border-r border-border bg-secondary/40 py-4 shadow-md">
-      <ThemeToggle />
+    <div className="flex h-screen w-20 flex-col items-center gap-4 border-r border-border bg-background py-4 shadow-md">
+      <ThemeToggle variant="outline" />
       <Button variant="outline" size="icon">
         <Cog />
       </Button>
