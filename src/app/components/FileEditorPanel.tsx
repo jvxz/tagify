3754b7f54b001f38492120fdbcc6156b655ca2e6
jvxz <jamie@wisp.bio>
2024-11-29 20:12:31 -0500

    update FileEditorPanel.tsx, FileTree.tsx, FileTreePanel.tsx and Sidebar.tsx

--- src/app/components/FileEditorPanel.tsx
+++ src/app/components/FileEditorPanel.tsx
@@ -1,12 +1,22 @@
 "use client";
 import useFileStore from "@/lib/store/files";
-
+import { Button } from "@/components/ui/button";
 export default function FileEditorPanel() {
   const { selectedFile } = useFileStore();
 
   return (
-    <div className="p-8">
-      <p className="text-4xl font-bold">{selectedFile}</p>
+    <div className="flex h-16 items-center justify-between border-b border-border/50 p-4">
+      <p className="text-xl font-bold">
+        {selectedFile ? selectedFile : "no file selected"}
+      </p>
+      <div className="flex items-center gap-2">
+        <Button isDisabled={!selectedFile} variant="destructive" size="sm">
+          discard
+        </Button>
+        <Button isDisabled={!selectedFile} size="sm">
+          save
+        </Button>
+      </div>
     </div>
   );
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { Cog } from "lucide-react";
+import { Cog, Info } from "lucide-react";
 import ThemeToggle from "./ThemeToggle";
 
 export default function Sidebar() {
   return (
-    <div className="flex h-screen w-20 flex-col items-center gap-4 border-r border-border bg-sidebar py-4">
+    <div className="flex h-screen w-20 flex-col items-center gap-4 border-r border-border bg-secondary/40 py-4">
       <ThemeToggle />
       <Button variant="outline" size="icon">
         <Cog />
+      </Button>
+      <Button variant="outline" size="icon">
+        <Info />
       </Button>
     </div>
   );
