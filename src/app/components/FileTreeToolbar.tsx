ffd96521eb17a8cb9ba64c1be3ee52e2120eaf1a
jvxz <jamie@wisp.bio>
2024-11-30 21:36:01 -0500

    update .gitignore, FileTreeToolbar.tsx, FileTreeToolbarSort.tsx and page.tsx

--- src/app/components/FileTreeToolbar.tsx
+++ src/app/components/FileTreeToolbar.tsx
@@ -1,43 +1,31 @@
 "use client";
 import { FileTreeSort } from "./FileTreeToolbarSort";
 import FileSelect from "./FileSelect";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { List, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import FileTreeToolbarResetButton from "./FileTreeToolbarResetButton";
+import FileTreeToolbarCheckboxToggle from "./FileTreeToolbarCheckboxToggle";
 
 export default function FileTreeBar() {
-  const [barState, setBarState] = useState<"search" | "normal">("normal");
-
-  if (barState === "normal")
-    return (
-      <div className="flex gap-2 p-4">
-        <Button
+  return (
+    <div className="flex flex-col gap-4 px-4">
+      <div className="flex gap-2 pt-4">
+        {/* <Button
           onPress={() => setBarState("search")}
           variant="outline"
           size="icon"
         >
           <Search className="size-5" />
-        </Button>
-
-        <div className="flex-1">
+        </Button> */}
+        <div className="w-full flex-1">
           <FileTreeSort />
         </div>
+        <FileTreeToolbarCheckboxToggle />
         <FileTreeToolbarResetButton />
         <FileSelect />
       </div>
-    );
-  return (
-    <div className="flex items-center gap-4 p-4">
-      <Button
-        size="icon"
-        variant="outline"
-        onPress={() => setBarState("normal")}
-      >
-        <List className="size-5" />
-      </Button>
-      <Input placeholder="Search" />
+      <div className="z-50 flex h-fit items-center gap-2">
+        <Input placeholder="Search" className="h-10" />
+      </div>
     </div>
   );
 }
---
+++ src/app/components/FileTreeToolbarCheckboxToggle.tsx
@@ -0,0 +1,5 @@
+import { Checkbox } from "@/components/ui/checkbox";
+
+export default function FileTreeToolbarCheckboxToggle() {
+  return <Checkbox id="terms" />;
+}
