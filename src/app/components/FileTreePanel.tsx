b62a00afb2ce7e56d0fc8ecba5e4229dffe932f8
jvxz <jamie@wisp.bio>
2024-12-04 00:45:58 -0500

    Refactor Sidebar and FileTree components for improved layout and functionality; update RootLayout to include Sidebar. Clean up unused code in FileEditorPanelForm and adjust imports in FileTreePanel.

--- src/app/components/FileTreePanel.tsx
+++ src/app/components/FileTreePanel.tsx
@@ -4,15 +4,15 @@ import FileTreeToolbar from "./FileTreeToolbar";
 
 export default function FileTreePanel() {
   return (
-    <ScrollArea className="flex w-full flex-col">
+    <div className="flex h-full w-full flex-col pb-4">
       <div className="flex h-16 items-center justify-between border-b border-border/50 px-4">
         <p className="text-xl font-bold">tagify</p>
         <p>v0.0.1</p>
       </div>
       <FileTreeToolbar />
-      <div className="flex flex-col gap-2">
+      <ScrollArea className="flex flex-col gap-2">
         <FileTree />
-      </div>
-    </ScrollArea>
+      </ScrollArea>
+    </div>
   );
 }
