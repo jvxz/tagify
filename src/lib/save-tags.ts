691539b832648c6748bf37863b203612058096a7
jvxz <jamie@wisp.bio>
2024-12-05 15:59:24 -0500

    Update package.json and yarn.lock to add browser-id3-writer and mp3tag.js dependencies. Refactor FileEditorPanelForm and file store to utilize Tags type for improved metadata handling. This enhances type safety and maintains consistency across the application.

---
+++ src/lib/save-tags.ts
@@ -0,0 +1,17 @@
+import { ID3Writer } from 'browser-id3-writer';
+import { type Tags } from './types';
+
+export default async function saveTags(file: File, tags: Tags) {
+    const arrayBuffer = await file.arrayBuffer();
+    const writer = new ID3Writer(arrayBuffer);
+
+    if (tags.title) {
+        writer.setFrame("TIT2", tags.title);
+    }
+
+    writer.addTag();
+
+    const blob = writer.getBlob();
+    const url = writer.getURL();
+    return { blob, url };
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+export type Tags = {
+    artist: string | undefined;
+    title: string | undefined;
+    albumArtist: string | undefined;
+    album: string | undefined;
+    trackNumber: number | undefined;
+    totalTracks: number | undefined;
+    discNumber: number | undefined;
+    totalDiscs: number | undefined;
+    genre: () => string | undefined;
+    comments: () => string | undefined;
+    composer: () => string | undefined;
+    grouping: string | undefined;
+    year: number | undefined;
+}
