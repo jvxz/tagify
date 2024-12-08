2c655b4697fda5fc9a2bec71c64148c969fafe47
bewle <everbelle@proton.me>
2024-12-08 11:46:24 -0500

    update FileEditorPanelFormImage.tsx, FileSelect.tsx and tooltip-button.tsx

--- src/app/components/FileEditorPanelFormImage.tsx
+++ src/app/components/FileEditorPanelFormImage.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-import { ImagePlus } from "lucide-react";
+import { Download, ImageDown, ImagePlus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { type IAudioMetadata } from "music-metadata";
+import FileSelect from "./FileSelect";
+import { TooltipButton } from "@/components/ui/tooltip-button";
+import { saveAs } from "file-saver";
 
 export default function FileEditorPanelFormImage({
   data,
@@ -10,14 +13,33 @@ export default function FileEditorPanelFormImage({
 }) {
   if (data?.common?.picture?.[0]?.data != null)
     return (
-      <Image
-        src={`data:image/jpeg;base64,${Buffer.from(data.common.picture[0].data).toString("base64")}`}
-        blurDataURL={`data:image/jpeg;base64,${Buffer.from(data.common.picture[0].data).toString("base64")}`}
-        alt={data.common.album ?? "album cover"}
-        width={350}
-        height={350}
-        className="motion-preset-fade-sm rounded-md shadow-md"
-      />
+      <div className="group relative">
+        <Image
+          src={`data:image/jpeg;base64,${Buffer.from(data.common.picture[0].data).toString("base64")}`}
+          blurDataURL={`data:image/jpeg;base64,${Buffer.from(data.common.picture[0].data).toString("base64")}`}
+          alt={data.common.album ?? "album cover"}
+          width={350}
+          height={350}
+          className="motion-preset-fade-sm rounded-md border border-border shadow-md"
+        />
+        <div className="absolute left-0 top-0 z-10 flex size-full items-center justify-center gap-2 rounded-md bg-background/50 opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100">
+          <FileSelect mode="image" />
+          <TooltipButton
+            onPress={() => {
+              if (data?.common.picture?.[0]?.data)
+                saveAs(
+                  new Blob([data.common.picture[0].data], {
+                    type: "image/png",
+                  }),
+                  `${data.common.artist} - ${data.common.album}.png`,
+                );
+            }}
+            tooltip="Download cover"
+          >
+            <ImageDown className="size-5" />
+          </TooltipButton>
+        </div>
+      </div>
     );
   return (
     <div className="grid aspect-square w-full place-items-center rounded-md border-2 border-border">
--- src/app/components/FileSelect.tsx
+++ src/app/components/FileSelect.tsx
@@ -5,20 +5,29 @@ import useFileStore from "@/lib/store/files";
 import { TooltipButton } from "@/components/ui/tooltip-button";
 import { Import } from "lucide-react";
 
-function FileSelect() {
+function FileSelect({ mode }: { mode: "audio" | "image" }) {
   const { addFiles } = useFileStore();
   return (
     <FileTrigger
+      allowsMultiple={mode === "audio"}
       onSelect={(e) => {
         if (!e) return;
-        const files = Array.from(e).map((file) => ({
-          name: file.name,
-          file: file,
-        }));
-        addFiles(files);
+        if (mode === "audio") {
+          const files = Array.from(e).map((file) => ({
+            name: file.name,
+            file: file,
+          }));
+          addFiles(files);
+        }
+        if (mode === "image") {
+          console.log(e);
+        }
       }}
     >
-      <TooltipButton tooltip="Import">
+      <TooltipButton
+        tooltip={mode === "audio" ? "Import" : "New cover"}
+        className="size-10"
+      >
         <Import className="size-5" />
       </TooltipButton>
     </FileTrigger>
